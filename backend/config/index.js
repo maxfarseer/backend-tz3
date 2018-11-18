335c67f504a1143f901ff8d47070b2c0cc48a47d
Max P <maxfarseer@gmail.com>
2018-11-18 05:34:37 +0300

    remove lodash.merge, use Object.assign instead

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,8 +1,6 @@
-import merge from 'lodash.merge';
-import common from './env/common';
-
-const env = process.env.NODE_ENV || 'development';
+import common from "./env/common";
+const env = process.env.NODE_ENV || "development";
 /* eslint import/no-dynamic-require: 0 */
 const config = require(`./env/${env}`).default;
 
-export default merge(common, config);
+export default Object.assign(common, config);
